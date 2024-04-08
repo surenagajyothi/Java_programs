db8f232ec4db79ca51e304c3e8d53add52dd2b9d
surenagajyothi <nagajyothi8623@gmail.com>
2024-04-08 14:40:20 +0530

    floating numbers

---
+++ src/Float.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Float {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+       float num1= input.nextFloat();
+       float num2=input.nextFloat();
+       float mul=num1*num2;
+       System.out.println(mul);
+    }
+}
