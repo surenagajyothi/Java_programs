37558d2a004d5cb225cc97f5846b003a7c7008b5
surenagajyothi <nagajyothi8623@gmail.com>
2024-04-08 13:43:55 +0530

    enter the integer

---
+++ src/integer.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class integer {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        int num=input.nextInt();
+        System.out.println(num);
+    }
+}
